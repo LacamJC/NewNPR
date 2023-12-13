f6ef914a957e42e123535fe1ee03fd4d439a8e02
LacamJC <joaoramajo744@gmail.com>
2023-12-13 16:35:10 -0300

    Inserindo variaveis do Railway

--- database/conexao.js
+++ database/conexao.js
@@ -1,5 +1,17 @@
 const Sequelize = require('sequelize')
 
+const sequelize = new Sequelize("railway","root","5HdcA3--1EcF11ech6f1c2-4egc3A1Dg", {
+    dialect: "mysql",
+    host: "roundhouse.proxy.rlwy.net",
+    port: 26711
+})
+
+module.exports = sequelize
+
+
+/*
+const Sequelize = require('sequelize')
+
 const sequelize = new Sequelize("NewNPR","ramajo","", {
     dialect: "mysql",
     host: "localhost",
@@ -7,3 +19,4 @@ const sequelize = new Sequelize("NewNPR","ramajo","", {
 })
 
 module.exports = sequelize
+*/
