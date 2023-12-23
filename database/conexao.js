9222817fda36070438f4c7740869603446ab15d2
LacamJC <joaoramajo744@gmail.com>
2023-12-23 19:08:08 -0300

    Adicionando variaveis do railway

--- database/conexao.js
+++ database/conexao.js
@@ -1,21 +1,21 @@
-// const Sequelize = require('sequelize')
-
-// const sequelize = new Sequelize("railway","root","BEfAG-1hb1a211dC4FhH53EgbCb626Fg", {
-//     dialect: "mysql",
-//     host: "monorail.proxy.rlwy.net",
-//     port: 52009
-// })
-
-// module.exports = sequelize
-
-
-
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize("NewNPR","root","12345678", {
+const sequelize = new Sequelize("railway","root","4dGDh2a2Dd4fFhgeaC-F5fEfEa3E3h34", {
     dialect: "mysql",
-    host: "localhost",
-    port: 3306
+    host: "viaduct.proxy.rlwy.net",
+    port: 56170
 })
 
 module.exports = sequelize
+
+
+
+// const Sequelize = require('sequelize')
+
+// const sequelize = new Sequelize("NewNPR","ramajo","", {
+//     dialect: "mysql",
+//     host: "localhost",
+//     port: 3306
+// })
+
+// module.exports = sequelize
