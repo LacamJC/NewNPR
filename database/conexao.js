036a6423a78084b5a19b5001fe625066a9150397
LacamJC <joaoramajo744@gmail.com>
2024-01-04 17:32:14 -0300

    Arrumando responsividade das imagens

--- database/conexao.js
+++ database/conexao.js
@@ -1,21 +1,25 @@
-const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize("railway","root","Afe63BE4DaF3gH4G42F2CEaB1-B61-FC", {
-    dialect: "mysql",
-    host: "viaduct.proxy.rlwy.net",
-    port: 55409
-})
-
-module.exports = sequelize
 
 
 
 // const Sequelize = require('sequelize')
 
-// const sequelize = new Sequelize("NewNPR","ramajo","", {
-//     dialect: "mysql",
-//     host: "localhost",
-//     port: 3306
+// const sequelize = new Sequelize("railway","root","Afe63BE4DaF3gH4G42F2CEaB1-B61-FC", {
+    // dialect: "mysql",
+    // host: "viaduct.proxy.rlwy.net",
+    // port: 55409
 // })
 
 // module.exports = sequelize
+
+
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize("NewNPR","ramajo","", {
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306
+})
+
+module.exports = sequelize
