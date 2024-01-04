aea94f4cb53a2ee6e9a78fcd4efef045ecef2fb6
LacamJC <joaoramajo744@gmail.com>
2024-01-04 17:39:12 -0300

    Variaveis railway

--- database/conexao.js
+++ database/conexao.js
@@ -2,24 +2,24 @@
 
 
 
-// const Sequelize = require('sequelize')
-
-// const sequelize = new Sequelize("railway","root","Afe63BE4DaF3gH4G42F2CEaB1-B61-FC", {
-    // dialect: "mysql",
-    // host: "viaduct.proxy.rlwy.net",
-    // port: 55409
-// })
-
-// module.exports = sequelize
-
-
-
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize("NewNPR","ramajo","", {
+const sequelize = new Sequelize("railway","root","gb4Edd2-1h-34-hhfCge3hDEEFh-HdG5", {
     dialect: "mysql",
-    host: "localhost",
-    port: 3306
+    host: "monorail.proxy.rlwy.net",
+    port: 54154
 })
 
 module.exports = sequelize
+
+
+
+// const Sequelize = require('sequelize')
+
+// const sequelize = new Sequelize("NewNPR","ramajo","", {
+//     dialect: "mysql",
+//     host: "localhost",
+//     port: 3306
+// })
+
+// module.exports = sequelize
