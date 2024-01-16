4f6031b0edcb1a05ae628e385f2e211ca4d33e78
LacamJC <joaoramajo744@gmail.com>
2024-01-16 19:43:59 -0300

    Adicionando pasta para screenshots

--- database/conexao.js
+++ database/conexao.js
@@ -2,24 +2,24 @@
 
 
 
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize("railway","root","bFfgEA4Fd-Dag4Fg5f6bEhgD361faec2", {
-    dialect: "mysql",
-    host: "roundhouse.proxy.rlwy.net",
-    port: 59998
-})
-
-module.exports = sequelize
-
-
-
 // const Sequelize = require('sequelize')
 
-// const sequelize = new Sequelize("NewNPR","ramajo","", {
+// const sequelize = new Sequelize("railway","root","bFfgEA4Fd-Dag4Fg5f6bEhgD361faec2", {
 //     dialect: "mysql",
-//     host: "localhost",
-//     port: 3306
+//     host: "roundhouse.proxy.rlwy.net",
+//     port: 59998
 // })
 
 // module.exports = sequelize
+
+
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize("NewNPR","ramajo","", {
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306
+})
+
+module.exports = sequelize
