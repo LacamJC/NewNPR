67d06a53e2ada886c5a294dae1b19880314320f4
LacamJC <joaoramajo744@gmail.com>
2024-02-05 17:19:15 -0300

    Add roles

--- database/conexao.js
+++ database/conexao.js
@@ -2,24 +2,24 @@
 
 
 
-// const Sequelize = require('sequelize')
-
-// const sequelize = new Sequelize("railway","root","fG1A3hDeD4Hcefa5h1HBcG3bHBGe6G4e", {
-//     dialect: "mysql",
-//     host: "roundhouse.proxy.rlwy.net",
-//     port: 56019
-// })
-
-// module.exports = sequelize
-
-
-
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize("NewNPR","ramajo","123456", {
+const sequelize = new Sequelize("railway","root","a6Bd6-hC2Cad4BFAeg2ahH12hCcHc63b", {
     dialect: "mysql",
-    host: "localhost",
-    port: 3306
+    host: "rviaduct.proxy.rlwy.net",
+    port: 52417
 })
 
 module.exports = sequelize
+
+
+
+// const Sequelize = require('sequelize')
+
+// const sequelize = new Sequelize("NewNPR","ramajo","123456", {
+//     dialect: "mysql",
+//     host: "localhost",
+//     port: 3306
+// })
+
+// module.exports = sequelize
