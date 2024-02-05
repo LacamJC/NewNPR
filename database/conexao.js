ba0ba752620622dead529d888f096686ce6308b8
LacamJC <joaoramajo744@gmail.com>
2024-02-05 15:47:59 -0300

    Add shadows

--- database/conexao.js
+++ database/conexao.js
@@ -2,24 +2,24 @@
 
 
 
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize("railway","root","fG1A3hDeD4Hcefa5h1HBcG3bHBGe6G4e", {
-    dialect: "mysql",
-    host: "roundhouse.proxy.rlwy.net",
-    port: 56019
-})
-
-module.exports = sequelize
-
-
-
 // const Sequelize = require('sequelize')
 
-// const sequelize = new Sequelize("NewNPR","ramajo","123456", {
+// const sequelize = new Sequelize("railway","root","fG1A3hDeD4Hcefa5h1HBcG3bHBGe6G4e", {
 //     dialect: "mysql",
-//     host: "localhost",
-//     port: 3306
+//     host: "roundhouse.proxy.rlwy.net",
+//     port: 56019
 // })
 
 // module.exports = sequelize
+
+
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize("NewNPR","ramajo","123456", {
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306
+})
+
+module.exports = sequelize
