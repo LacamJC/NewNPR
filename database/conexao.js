6a37ebd995d23eec28bf944f9fa28debcb9ccc42
LacamJC <joaoramajo744@gmail.com>
2024-02-05 22:04:58 -0300

    Add railway

--- database/conexao.js
+++ database/conexao.js
@@ -2,24 +2,24 @@
 
 
 
-// const Sequelize = require('sequelize')
-
-// const sequelize = new Sequelize("railway","root","a6Bd6-hC2Cad4BFAeg2ahH12hCcHc63b", {
-//     dialect: "mysql",
-//     host: "viaduct.proxy.rlwy.net",
-//     port: 52417
-// })
-
-// module.exports = sequelize
-
-
-
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize("NewNPR","ramajo","123456", {
+const sequelize = new Sequelize("railway","root","a6H1CB4Ec-6AgEae5FdeBC5FgH46542A", {
     dialect: "mysql",
-    host: "localhost",
-    port: 3306
+    host: "roundhouse.proxy.rlwy.net",
+    port: 38970
 })
 
 module.exports = sequelize
+
+
+
+// const Sequelize = require('sequelize')
+
+// const sequelize = new Sequelize("NewNPR","ramajo","123456", {
+//     dialect: "mysql",
+//     host: "localhost",
+//     port: 3306
+// })
+
+// module.exports = sequelize
